96de4d9f48b8c251fae498abc2a7056f92bbbca1
kunalsah <kunalsahu.6841@gmail.com>
2022-08-24 10:31:35 +0530

    add3

--- add3.cpp
+++ add3.cpp
@@ -2,10 +2,10 @@
 #include<conio.h>
 int main()
 {
-    int a,b,c=0;
+    int a,b,c=0,d;
     cout<<"enter tow number";
-    cin>>a>>b;
-    c=a+b;
+    cin>>a>>b>>d;
+    c=a+b+d;
     cout<<c;
     getch();
     return 0;
