8a9f2167b77153a9f39892453ee70248772ff155
kunalsah <kunalsahu.6841@gmail.com>
2022-08-24 11:10:02 +0530

    updated

--- add3.cpp
+++ add3.cpp
@@ -1,12 +1,25 @@
-#include<iostream.h>
-#include<conio.h>
+#include<iostream>
+using namespace std;
+// Function to check if a number is a palindrome or not.
+int Palindrome(int n)
+{
+// Find reverse of n
+int reverse = 0;
+for (int i = n; i > 0; i /= 10)
+reverse = reverse*10 + i%10;
+// To check if they are same
+return (n==reverse);
+}
+//function to prints palindrome between a minimum and maximum number
+void countPalindrome(int minimum, int maximum)
+{
+for (int i = minimum ; i <= maximum; i++)
+if (Palindrome(i))
+cout << i << " ";
+}
+// program to test above functionality
 int main()
 {
-    int a,b,c=0,d;
-    cout<<"enter tow numberalidfuwoafow";
-    cin>>a>>b>>d;
-    c=a+b+d;
-    cout<<c;
-    getch();
-    return 0;
+countPalindrome(100,2000);
+return 0;
 }
